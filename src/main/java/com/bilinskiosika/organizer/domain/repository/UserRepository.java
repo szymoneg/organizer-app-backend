4b54a5385e306b5c687d429944177e7079e8c7b7
szymoneg <szymon.zsp5@gmail.com>
2021-06-02 00:28:26 +0200

    hotfix in repo

--- src/main/java/com/bilinskiosika/organizer/domain/repository/UserRepository.java
+++ src/main/java/com/bilinskiosika/organizer/domain/repository/UserRepository.java
@@ -11,4 +11,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long>, CrudRepository<User, Long> {
     Optional<User> findByUsername(String username);
     User findByIdUser(long idUser);
+    User findUserByUsername(String username);
 }
